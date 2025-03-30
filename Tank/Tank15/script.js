8a9d0513c806e4bf03965690484c5454be03b151
thangcph44444 <congthangdajk@gmail.com>
2025-03-30 23:22:54 +0700

    hja

--- Tank/Tank15/script.js
+++ Tank/Tank15/script.js
@@ -1,9 +1,11 @@
+let a = parseFloat(prompt("nhập số a :"))
+let b = parseFloat(prompt("nhập số b :"))
+
+
 function isSameSign(a, b) {
     // Kiểm tra nếu cả hai số đều dương hoặc cả hai số đều âm
     return (a >= 0 && b >= 0) || (a < 0 && b < 0);
 }
 
 // Test cases
-console.log(isSameSign(10, 20)); // true
-console.log(isSameSign(-10, -20)); // true
-console.log(isSameSign(10, -20)); // false
+console.log(isSameSign(a,b)); // true
