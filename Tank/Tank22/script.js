3d396e6786af430769b8e7af155a340774ee8703
Thang200324 <congthangdajk@gmail.com>
2025-04-08 21:31:43 +0700

    ,\

--- Tank/Tank22/script.js
+++ Tank/Tank22/script.js
@@ -2,10 +2,10 @@ const arrayNumber = [1, 2, 3, 4, 3, 2, 1];
 const testArrays = [ [1, 2, 3, 3, 2, 1] ,[1, 2, 3, 2, 1]] 
 function checkSymmetricalArr(arr) {
     if (!Array.isArray(arr)) {
-        return "Dữ liệu không hợp lệ";
+        return "dữ liệu không hợp lệ";
     }
     if (arr.length === 1) {
-        return "Mảng có 1 phần tử, không kiểm tra được";
+        return "mảng có 1 phần tử không kiểm tra được";
     }
     for (let i = 0; i < (arr.length - 1) / 2; i++) {
         if (arr[i] !== arr[arr.length - 1 - i]) {
